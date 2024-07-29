90e9cb63bb0366996925ee9eb082d213ab041b7b
Lilas Lounes <49814930+lilas74@users.noreply.github.com>
2024-07-29 15:06:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: "Jhon",
+        age: 22,
+        skills: ["C", "C++", "Java", "Python", "Ruby", "JavaScript"]
+    };
+    return member;
+}
